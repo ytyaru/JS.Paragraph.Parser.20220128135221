759d5c50be7f31f30d5acc37bf5e6064fa5bc3c7
ytyaru <ytyaru@outlook.jp>
2022-01-29 14:57:36 +0900

    0.7 ParseSet追加（複数行をClassで指定するヤツのmargin,border,padding版）

---
+++ src/0.7/style.js
@@ -0,0 +1,21 @@
+class Style {
+    constructor(id) {
+        this._id = id;
+        this._addStyleElement()
+    }
+    get Id() { return this._id; }
+    add(css) {
+        const style = document.getElementById(this._id);
+        if (!style.innerText.includes(css)) {
+            style.appendChild(document.createTextNode(css));
+        }
+    }
+    _addStyleElement() {
+        const head = document.getElementsByTagName('head').item(0);
+        const style = document.createElement('style');
+        style.setAttribute('id', this._id)
+        style.media = 'screen';
+        style.type = 'text/css';
+        head.appendChild(style);
+    }
+}
