b3a74c4ba7b040aa88c1ef41445183cda036584f
ytyaru <ytyaru@outlook.jp>
2022-01-30 21:21:44 +0900

    0.13 途中。アンカー見出しを実装したい。

---
+++ src/0.13/js/parser/heading-parse-set.js
@@ -0,0 +1,44 @@
+class HeadingInput extends RegExpParseInput { // <em>強調</em>  ＊強調＊
+    constructor(options={}) {
+        super();
+        super.RegExps.push(new RegExp(`^([#＃]{1,})([^\n]+?)$`, 'gm'));
+    }
+    get Options() { return super.Options; }
+    get RegExps() { return super.RegExps; }
+}
+class HeadingOutput extends RegExpParseOutput {
+    constructor() {
+        super((match, meta, content)=>{
+            console.log('meta:', meta)
+            console.log('content:', content)
+            const level = Math.max(...(['#','＃'].map(m=>(meta.match(new RegExp(`${m}`, 'g')) || []).length)))
+            return (level < 7) ? ElementString.get(`h${level}`, content) : match;
+        });
+    }
+    parse(text, regexp) {
+        return text.replace(regexp, (match, meta, content, offset, string, groups)=>{
+            return this._func(match, meta, content);
+        });
+    }
+
+}
+class AnchorHeadingOutput extends RegExpParseOutput {
+    constructor() {
+        super((match, meta, content)=>{
+            console.log('meta:', meta)
+            console.log('content:', content)
+            const level = Math.max(...(['#','＃'].map(m=>(meta.match(new RegExp(`${m}`, 'g')) || []).length)))
+            return (level < 7) ? ElementString.get(`h${level}`, content) : match;
+        });
+    }
+    parse(text, regexp) {
+        return text.replace(regexp, (match, meta, content, offset, string, groups)=>{
+            return this._func(match, meta, content);
+        });
+    }
+
+}
+class HeadingParseSetFactory {
+    static #Normal = new RegExpParseSet(new HeadingInput(), new HeadingOutput());
+    static get Normal() { return HeadingParseSetFactory.#Normal; } 
+}
