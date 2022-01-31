0803f663541391de82c362754627590435f61875
ytyaru <ytyaru@outlook.jp>
2022-01-31 12:07:56 +0900

    0.15 heading追加をdocs/に追加。

---
+++ docs/0.15/js/parser/parse-set-factory.js
@@ -0,0 +1,18 @@
+class ParseSetFactory {
+    static #Novel = [
+        HeadingParseSetFactory.Named, 
+        ParagraphParseSetFactory.Book, 
+        RubyParseSetFactory.RootHtml, 
+        EmParseSetFactory.Normal
+    ];
+    static #Poem = [
+        HeadingParseSetFactory.Named, 
+        ParagraphParseSetFactory.PoemMargin, 
+        RubyParseSetFactory.RootHtml, 
+        EmParseSetFactory.Normal
+    ];
+    //static #Blog = [...];
+
+    static get Novel() { return ParseSetFactory.#Novel; } 
+    //static get Blog() { return ParseSetFactory.#Blog; } 
+}
